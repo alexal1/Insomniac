6626e043bf473ab7352fe34dc0af0611633330d4
Alexander Mishchenko <admishchenko@yandex-team.ru>
2020-08-17 15:50:55 +0300

    Another try to fix navigation

--- src/interaction_rect_checker.py
+++ src/interaction_rect_checker.py
@@ -7,20 +7,34 @@ _tab_bar_top = None
 def update_interaction_rect(device):
     action_bar = device(resourceId='com.instagram.android:id/action_bar_container',
                         className='android.widget.FrameLayout')
-    global _action_bar_bottom
-    _action_bar_bottom = action_bar.bounds['bottom']
+    if action_bar.exists:
+        global _action_bar_bottom
+        _action_bar_bottom = action_bar.bounds['bottom']
+    else:
+        print(COLOR_FAIL + "Cannot find action bar. This can lead to crashes, but script will continue working."
+              + COLOR_ENDC)
 
     tab_bar = device(resourceId='com.instagram.android:id/tab_bar',
                      className='android.widget.LinearLayout')
-    global _tab_bar_top
-    _tab_bar_top = tab_bar.bounds['top']
+    if tab_bar.exists:
+        global _tab_bar_top
+        _tab_bar_top = tab_bar.bounds['top']
+    else:
+        print(COLOR_FAIL + "Cannot find tab bar. This can lead to crashes, but script will continue working."
+              + COLOR_ENDC)
 
 
 def is_in_interaction_rect(view):
     if _action_bar_bottom is None or _tab_bar_top is None:
         print(COLOR_FAIL + "Interaction rect is not specified in interaction_rect_checker.py" + COLOR_ENDC)
-        return False
 
     view_top = view.bounds['top']
     view_bottom = view.bounds['bottom']
-    return _action_bar_bottom <= view_top and view_bottom <= _tab_bar_top
+
+    if _action_bar_bottom is not None and view_top > _action_bar_bottom:
+        return False
+
+    if _tab_bar_top is not None and view_bottom < _tab_bar_top:
+        return False
+
+    return True
--- src/navigation.py
+++ src/navigation.py
@@ -4,42 +4,37 @@ from src.utils import *
 
 
 def navigate(device, tab):
+    if not _do_navigate(device, tab):
+        print("Trying to find profile button directly...")
+        profile_button = device(resourceId='com.instagram.android:id/profile_tab',
+                                className='android.widget.FrameLayout')
+        if not profile_button.exists:
+            print(COLOR_FAIL + "Cannot find profile button. No idea what to do, sorry..." + COLOR_ENDC)
+            raise NavigationException()
+
+        # Two clicks to reset tab content
+        profile_button.click.wait()
+        profile_button.click.wait()
+
+        if not tab == Tabs.PROFILE:
+            _do_navigate(device, tab)
+
+
+def _do_navigate(device, tab):
     tab_name = tab.name.lower()
     tab_index = tab.value
 
     print("Press " + tab_name)
     tab_bar = device(resourceId='com.instagram.android:id/tab_bar', className='android.widget.LinearLayout')
     if not tab_bar.exists:
-        _navigate_alternative(device, tab)
-        return
+        print("Cannot find tab bar.")
+        return False
     button = tab_bar.child(index=tab_index)
 
     # Two clicks to reset tab content
     button.click.wait()
     button.click.wait()
-
-
-def _navigate_alternative(device, tab):
-    print("Standard way of navigation failed, trying another one...")
-    if tab == Tabs.HOME:
-        button = device(description='Home', className='android.widget.FrameLayout')
-    elif tab == Tabs.SEARCH:
-        button = device(description='Search and Explore', className='android.widget.FrameLayout')
-    elif tab == Tabs.PLUS:
-        button = device(description='Camera', className='android.widget.FrameLayout')
-    elif tab == Tabs.LIKES:
-        button = device(description='Activity', className='android.widget.FrameLayout')
-    else:
-        button = device(description='Profile', className='android.widget.FrameLayout')
-
-    if not button.exists:
-        tab_name = tab.name.lower()
-        print(COLOR_FAIL + "Cannot find " + tab_name + " button. Switch to English please." + COLOR_ENDC)
-        raise NavigationException("Cannot find " + tab_name + " button.")
-
-    # Two clicks to reset tab content
-    button.click.wait()
-    button.click.wait()
+    return True
 
 
 @unique
