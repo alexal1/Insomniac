c5095c0b86ae58c67c77909a00c53ea04e0e6d78
Alexander Mishchenko <admishchenko@yandex-team.ru>
2020-08-18 23:06:37 +0300

    Start from bottom in --remove-mass-followers

--- src/action_remove_mass_followers.py
+++ src/action_remove_mass_followers.py
@@ -7,11 +7,17 @@ def remove_mass_followers(device, max_followings, on_remove, storage):
     followers_button = device(resourceId='com.instagram.android:id/row_profile_header_followers_container',
                               className='android.widget.LinearLayout')
     followers_button.click.wait()
+    _scroll_to_bottom(device)
 
     _iterate_over_followers(device, max_followings, on_remove, storage)
 
 
 def _iterate_over_followers(device, max_followings, on_remove, storage):
+    def scrolled_to_top():
+        row_search = device(resourceId='com.instagram.android:id/row_search_edit_text',
+                            className='android.widget.EditText')
+        return row_search.exists
+
     need_to_restart = False  # after removing a user we need to start the screen from the beginning
     while True:
         print("Iterate over visible followers")
@@ -37,6 +43,7 @@ def _iterate_over_followers(device, max_followings, on_remove, storage):
                     print("@" + username + " is in whitelist, skip.")
                     continue
 
+                print("Open @" + username)
                 user_name_view.click.wait()
                 random_sleep()
                 is_mass_follower = _is_mass_follower(device, username, max_followings)
@@ -65,11 +72,14 @@ def _iterate_over_followers(device, max_followings, on_remove, storage):
             need_to_restart = False
             continue
 
-        if screen_iterated_followers > 0:
+        if scrolled_to_top():
+            print(COLOR_OKGREEN + "Scrolled to top, finish." + COLOR_ENDC)
+            return
+        elif screen_iterated_followers > 0:
             print(COLOR_OKGREEN + "Need to scroll now" + COLOR_ENDC)
             list_view = device(resourceId='android:id/list',
                                className='android.widget.ListView')
-            list_view.scroll.toEnd(max_swipes=1)
+            list_view.scroll.toBeginning(max_swipes=1)
         else:
             print(COLOR_OKGREEN + "No followers were iterated, finish." + COLOR_ENDC)
             return
@@ -92,3 +102,27 @@ def _close_dialog_if_shown(device):
     remove_button = dialog_root_view.child(index=0).child(resourceId='com.instagram.android:id/primary_button',
                                                           className='android.widget.TextView')
     remove_button.click.wait()
+
+
+def _scroll_to_bottom(device):
+    print("Scroll to bottom")
+
+    def is_end_reached():
+        see_all_button = device(resourceId='com.instagram.android:id/see_all_button',
+                                className='android.widget.TextView')
+        return see_all_button.exists
+
+    list_view = device(resourceId='android:id/list',
+                       className='android.widget.ListView')
+    while not is_end_reached():
+        list_view.fling.toEnd(max_swipes=1)
+
+    print("Scroll back to the first follower")
+
+    def is_at_least_one_follower():
+        follower = device(resourceId='com.instagram.android:id/follow_list_container',
+                          className='android.widget.LinearLayout')
+        return follower.exists
+
+    while not is_at_least_one_follower():
+        list_view.scroll.toBeginning(max_swipes=1)
