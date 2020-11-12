554ee87022571d79bde8c4b2242d051c654c4e92
Alexander Mishchenko <admishchenko@yandex-team.ru>
2020-11-12 15:03:33 +0300

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 # Run this bash script to publish the package to PyPi
 test=True
+version=$(sed -n "s/.*__version__ = .\([0-9.]*\).*/\1/p" ./insomniac/__version__.py)
+echo "Publishing: test=$test, version=$version"
 
-python encoder.py
+python3 stubs_generator.py
+git tag v"$version"
+git push --tags
 python3 setup.py sdist bdist_wheel
 if $test; then
   python3 -m twine upload --repository testpypi dist/*
