ea0481c94bb96351aee0d8487c0af12243e78b2c
EAleixandre <ealeixandredev@gmail.com>
2021-09-26 15:40:26 +0200

    Removes breakpoint

--- insomniac/actions_impl.py
+++ insomniac/actions_impl.py
@@ -801,7 +801,7 @@ def do_unfollow(device, my_username, username, storage, check_if_is_follower, us
         print(f"Unfollowing @{username}...")
         unfollow_button.click()
         sleeper.random_sleep()
-    breakpoint()
+
     unfollow_confirmed = False
     dialog_view = DialogView(device)
     if dialog_view.is_visible():
